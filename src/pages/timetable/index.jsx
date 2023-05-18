af22711e595035be60cb321e477dea37c032fdde
asheroh <wnstj701@naver.com>
2023-05-19 01:43:56 +0900

    Design: 타임테이블 날짜 박스 레이아웃

--- src/pages/timetable/index.jsx
+++ src/pages/timetable/index.jsx
@@ -1,13 +1,65 @@
 import styled from "styled-components";
 
-const Container = styled.div`
-  
-`
+const primaryColor = "#FC8CAE";
+const secondaryColor = "#525252";
 
-export default function TimeTable(){
-  return(
-    <Container>
-      여기는 타입테이블 페이지 입니다 하항
-    </Container>
-  )
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid black;
+  z-index: 1;
+`;
+
+const DateSection = styled.section`
+  width: 100%;
+  height: 120px;
+  border: 1px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  gap: 30px;
+`;
+
+const DateBox = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
+  font-weight: 900;
+`;
+
+const DateNum = styled.span`
+  font-size: 36px;
+  text-shadow: 2px 2px 0px #ffffff, 5px 4px 0px rgba(0, 0, 0, 0.15);
+  color: ${primaryColor};
+`;
+
+const DateWeek = styled.span`
+  font-size: 14px;
+`;
+
+export default function TimeTable() {
+  return (
+    <>
+      <Container>
+        <DateSection>
+          <DateBox>
+            <DateNum>24</DateNum>
+            <DateWeek>WED.</DateWeek>
+          </DateBox>
+          <DateBox>
+            <DateNum>25</DateNum>
+            <DateWeek>THR.</DateWeek>
+          </DateBox>
+        </DateSection>
+      </Container>
+      <style jsx>{`
+        * {
+          font-family: "yg-jalnan";
+        }
+      `}</style>
+    </>
+  );
 }
