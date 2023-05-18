b94ee005a99094e7acbad4aa4851d8371bbe221c
asheroh <wnstj701@naver.com>
2023-05-19 03:21:16 +0900

    Design: Setting TimeTable page toggle UI

--- src/pages/timetable/index.jsx
+++ src/pages/timetable/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const primaryColor = "#FC8CAE";
@@ -7,14 +8,12 @@ const Container = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
-  border: 1px solid black;
   z-index: 1;
 `;
 
 const DateSection = styled.section`
   width: 100%;
   height: 120px;
-  border: 1px solid black;
   display: flex;
   justify-content: center;
   align-items: flex-end;
@@ -34,13 +33,55 @@ const DateNum = styled.span`
   font-size: 36px;
   text-shadow: 2px 2px 0px #ffffff, 5px 4px 0px rgba(0, 0, 0, 0.15);
   color: ${primaryColor};
+  font-family: "yg-jalnan";
 `;
 
 const DateWeek = styled.span`
   font-size: 14px;
+  font-family: "yg-jalnan";
+`;
+
+const IconBox = styled.section`
+  margin: 0 10px;
+  font-size: 24px;
+`;
+
+const ToggleHeader = styled.section`
+  width: 100%;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  font-family: "yg-jalnan";
+`;
+
+const ToggleBox = styled.section`
+  width: 100%;
+  height: ${(props) => (props.isOpen ? "45vh" : "60px")};
+  display: flex;
+  box-shadow: 3px 3px 20px -10px rgba(0, 0, 0, 0.7);
+  border-radius: 30px;
+  background-color: #fff;
+  color: ${primaryColor};
+  font-weight: 700;
+  padding-left: 15px;
+  transition: all 0.5s;
+  cursor: pointer;
 `;
 
 export default function TimeTable() {
+  // State 관리----------------------------------------
+  const [performance, setPerformance] = useState(false);
+  const [specialGuest, setSpecialGuest] = useState(true);
+
+  // Function 관리-------------------------------------
+  const handleShrink = () => {
+    setPerformance(!performance);
+  };
+  const handleGrow = () => {
+    setSpecialGuest(!specialGuest);
+  };
+
+  //
   return (
     <>
       <Container>
@@ -54,6 +95,20 @@ export default function TimeTable() {
             <DateWeek>THR.</DateWeek>
           </DateBox>
         </DateSection>
+        <br /> <br />
+        <ToggleBox onClick={handleShrink} isOpen={performance}>
+          <ToggleHeader>
+            <IconBox>▶</IconBox>
+            PERFORMANCE
+          </ToggleHeader>
+        </ToggleBox>
+        <br />
+        <ToggleBox onClick={handleGrow} isOpen={specialGuest}>
+          <ToggleHeader>
+            <IconBox>▼</IconBox>
+            SPECIAL GUEST
+          </ToggleHeader>
+        </ToggleBox>
       </Container>
       <style jsx>{`
         * {
