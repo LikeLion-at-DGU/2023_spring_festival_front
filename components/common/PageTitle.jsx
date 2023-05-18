ccfd8b569ea35f46fc016e5e896431b02b41b69e
Lee Sangdon <88819145+leeideal@users.noreply.github.com>
2023-05-19 00:14:02 +0900

    feat: 공통 pageTitle,gradation

---
+++ components/common/PageTitle.jsx
@@ -0,0 +1,59 @@
+import { useRouter } from "next/router";
+import Image from 'next/image';
+import time from "../image/timetable.svg"
+import booth from "../image/booth.svg"
+import about from "../image/about.svg"
+import notice from "../image/notice.svg"
+import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+
+ 
+export default function PageTitle() {
+  const router = useRouter();
+  const [page, setPage] = useState(0);
+
+  useEffect(()=>{
+    if(router.pathname === "/timetable"){
+      setPage(1);
+    }else if(router.pathname === "/notice"){
+      setPage(2);
+    }else if(router.pathname === "/about"){
+      setPage(3);
+    }else if(router.pathname === "/booth"){
+      setPage(4);
+    }
+  })
+
+  const Img = styled(Image)`
+    width: 60%;
+    margin-top: 10px;
+  `
+  
+  return (
+    page === 1 ? 
+    <Img
+      src={time}
+      height={30}
+      alt={"title"}
+    /> : 
+    page === 2 ? 
+    <Img
+      src={notice}
+      height={30}
+      alt={"title"}
+    /> : 
+    page === 3 ? 
+    <Img
+      src={about}
+      height={30}
+      alt={"title"}
+    />  : 
+    page === 4 ? 
+    <Img
+      src={booth}
+      height={30}
+      alt={"title"}
+    />  : null
+    
+  );
+}
