939b783227e524faf0545a992cbe8be4ae8a668a
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-05-19 15:45:09 +0900

    feat : DateBox

--- src/pages/booth/index.jsx
+++ src/pages/booth/index.jsx
@@ -1,13 +1,56 @@
-import styled from "styled-components";
+import { React, useState} from "react";
 
-const Container = styled.div`
-  
-`
+
+import styled from "styled-components";
+import { motion } from 'framer-motion';
+import { DateNum, DateWeek } from "../timetable";
+import { BoxDate, BoxDay, Container, DateContainer, DayBox } from "./style";
+
+
+// 날짜 배열 
+const dayArray = [
+  {
+    id: 1,
+    date: 23,
+    day: 'TUE',
+  },
+  {
+    id: 2,
+    date: 24,
+    day: 'WED',
+  },
+  {
+    id: 3,
+    date: 25,
+    day: 'THR',
+  },
+];
 
 export default function Booth(){
+  // DATE 관련 
+  // 27일부터 30일까지
+  const day = new Date();
+  const todate = day.getDate() - 23 === 2 ? 2 : day.getDate() - 23 === 3 ? 3 : 1;
+  
+  const [isToday, setIsToday] = useState(todate);
+
+
+
   return(
     <Container>
-      여기는 부스 페이지 입니다 하항!
+       <DateContainer>
+        {/* api호출 방법 :/api/booths?day={day}&location={location} */}
+        {dayArray.map((i) => (
+          <DayBox key={i.id} onClick={() => setIsToday(i.id)}>
+            
+            <BoxDate isActive={isToday === i.id}><DateNum>{i.date}</DateNum></BoxDate>
+            <BoxDay isActive={isToday === i.id}><DateWeek>{i.day}</DateWeek></BoxDay>
+          </DayBox>
+        ))}
+      </DateContainer>
+
+
+      
     </Container>
   )
 }
---
+++ src/pages/booth/style.jsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { motion } from 'framer-motion';
+
+export const Container = styled.div`
+  width: 100%;
+  text-align: center;
+  padding: 2rem 0rem 9rem 0;
+  
+`
+// 날짜 컨테이너
+export const DateContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+`;
+
+export const DayBox = styled.div`
+  bottom: 0;
+  margin: 0px 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: -2px;
+`;
+
+export const BoxDate = styled.span`
+  opacity: ${(props) => (props.isActive ? 1 : 0.9)};
+  transition: all 0.3s;
+  margin-bottom: 5px;
+  color: ${(props) => (props.isActive ? '#FC8CAE' : '#525252')};
+`;
+
+export const BoxDay = styled.span`
+  font-size: 21px;
+  margin-bottom: 7px;
+  transition: all 0.3s;
+  color: ${(props) => (props.isActive ? '#FC8CAE' : '#525252')};
+`;
