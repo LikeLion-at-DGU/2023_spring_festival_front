396c76dffde74482d08ec25ee8a4414ab523009b
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-05-20 23:35:55 +0900

    feat: 검색기능 구현

---
+++ components/booth/BoothCard.jsx
@@ -0,0 +1,9 @@
+function BoothCard() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default BoothCard;
--- src/pages/booth/search_style.jsx
+++ src/pages/booth/search_style.jsx
@@ -2,3 +2,18 @@ import styled from "styled-components";
 import Image from "next/image";
 import { motion } from 'framer-motion';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+
+export const SearchContentWrapper = styled.div`
+margin-top: 60px;
+margin-bottom: 30px;
+color:#525252;
+font-size: 20px;
+font-weight: 600;
+z-index: 1;
+
+`
+export const SearchContentHeader = styled.h1`
+    
+`
