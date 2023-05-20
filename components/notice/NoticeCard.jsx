ba659a7dd3ae9ce80c49c517b06e13357006711c
Seulgigi <2021112414@dgu.ac.kr>
2023-05-20 18:57:37 +0900

    feat: notice 리스트 UI

---
+++ components/notice/NoticeCard.jsx
@@ -0,0 +1,83 @@
+import styled from "styled-components";
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Container = styled.div`
+    box-shadow: 2px 2px 7px rgba(0, 0, 0, 0.15);
+    z-index: 5;
+    background: #FFF;
+    border-radius: 10px;
+    padding: 25px;
+    margin: 3% 0;
+    /* font-family: 'AppleSDGothicNeoM00'; */
+`;
+
+const Header = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const Title = styled.div`
+    color: #525252;
+    font-weight: 400;
+    font-size: 20px;
+    /* font-family: 'AppleSDGothicNeoB00'; */
+`;
+
+const Date = styled.div`
+    font-weight: 400;
+    font-size: 12px;
+    color: #979797;
+`;
+
+const Body = styled.div`
+    font-weight: 400;
+    font-size: 14px;
+    color: #525252;
+    margin: 5% 0;
+    line-height: 120%;
+`;
+
+const ImgBox = styled.div`
+    margin: 5px 0;
+    display: flex;
+    flex-direction: row;
+    justify-items: center;
+    align-items: center;
+`;
+
+const NoticeCard = ({title, content, created_at, images}) => {
+
+    const date = created_at.slice(0,10);
+
+    console.log(images)
+
+    return(
+        <Container>
+            <Header>
+                <Title>{title}</Title>
+                <Date>{date}</Date>
+            </Header>
+            <Body>
+                { content.length <= 60 ? 
+                    <>{content}</> : <>{content.slice(0,60)}...</>
+                }
+            </Body>
+            { ( images && images.length > 0 ) &&
+            <ImgBox>
+                {images.map((image)=>{
+                    <Image
+                        key={image.id}
+                        src={image.url}
+                        alt={image.alt}
+                    />
+                })}
+            </ImgBox>
+            }
+            
+        </Container>
+    );
+};
+
+export default NoticeCard;
--- src/pages/notice/index.jsx
+++ src/pages/notice/index.jsx
@@ -1,13 +1,134 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import NoticeCard from "components/notice/NoticeCard";
 
 const Container = styled.div`
-  
-`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 25px;
+  z-index: 1;
+`;
+
+const TypeWrapper = styled.div`
+  width: 100%;
+  justify-content: center;
+  display: flex;
+`;
+
+const Type = styled.button`
+  width: 20%;
+  padding: 3% 3%;
+  background: transparent;
+  border: none;
+  border-bottom: 2px solid ${(props)=>props.isSelected ? '#FC8CAE' : '#979797'};
+  color: ${(props)=>props.isSelected ? '#FC8CAE' : '#979797'};
+  font-size: 18px;
+  line-height: 19px;
+`;
+
+const TypeBody = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  margin: 7% 0;
+`;
 
 export default function Booth(){
+
+  // 타입
+  const [currentType, setCurrentType] = useState('전체');
+  const type = ['전체', '주요', '축제', '이벤트', '기타'];
+  const selectTypeHandler = (type) => {
+    setCurrentType(type);
+    console.log(currentType);
+  };
+
+  // notice list fetch
+  const [notice, setNotice] = useState([]);
+  const fetchNotice = async() => {
+    try {
+      // dummy
+      const noticeData = [
+        {
+          "id": 1,
+          "title": "main",
+          "type": "주요",
+          "content": "글이 길면 어때? 글이 길면 어때? 글이 길면 어때? 글이 길면 어때? 글이 길면 어때? 글이 길면 어때? 글이 길면 어때? 글이 길면 어때? 글이 길면 어때?",
+          "created_at": "2023-05-13T07:47:07.687842+09:00",
+          "images": []
+        },
+        {
+          "id": 2,
+          "title": "festival",
+          "type": "축제",
+          "content": "festival ㅅㅂ!",
+          "created_at": "2023-05-13T07:47:07.687842+09:00",
+          "images": []
+        },
+        {
+          "id": 3,
+          "title": "event",
+          "type": "이벤트",
+          "content": "글이 길면 어때? 글이 길면 어때? 글이 길면 어때? 글이 길면 어때? 글이 길면 어때? 글이 길면 어때? 글이 길면 어때? 글이 길면 어때? 글이 길면 어때?",
+          "created_at": "2023-05-13T07:47:07.687842+09:00",
+          "images": []
+        },
+        {
+          "id": 4,
+          "title": "etc",
+          "type": "기타",
+          "content": "etc ㅅㅂ!",
+          "created_at": "2023-05-13T07:47:07.687842+09:00",
+          "images": []
+        },
+      ];
+
+      setNotice(noticeData);
+    } catch (error) {
+      console.log('Error: ', error);
+    }
+  };
+
+  // notice list 불러오기
+  useEffect(()=>{
+    fetchNotice();
+  }, []);
+
+  const typeNotices = notice.filter((notice)=>{
+    if(currentType === '전체') {
+      return true;
+    } else {
+      return notice.type === currentType;
+    }
+  });
+
   return(
     <Container>
-      여기는 공지사항 페이지 입니다 하항
+      <TypeWrapper>
+        {type.map((type)=>{
+          return(
+            <Type
+              key={type}
+              onClick={()=>selectTypeHandler(type)}
+              isSelected={type===currentType}
+            >
+              {type}
+            </Type>
+          )
+        })}
+      </TypeWrapper>
+      <TypeBody>
+        {typeNotices.map((notice)=>(
+          <NoticeCard 
+            title={notice.title}
+            content={notice.content}
+            created_at={notice.created_at}
+            images={notice.images}
+          />
+        ))}
+        
+      </TypeBody>
     </Container>
   )
 }
