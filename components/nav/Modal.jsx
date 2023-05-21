114422bb19aa51194a3261b04976289ff84b770f
asheroh <wnstj701@naver.com>
2023-05-21 22:21:20 +0900

    Fix: Resolve modal z index issue

--- components/nav/Modal.jsx
+++ components/nav/Modal.jsx
@@ -1,120 +1,115 @@
 import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
 import React, { useState, useRef, useEffect } from "react";
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import nav_logo from "../image/common/nav_logo.svg";
 import {
-    ModalWrapper,
-    ModalOverlay,
-    ModalButton,
-    ModalHeader,
-    ModalTitle,
-    ModalPages,
-    ModalPageSection,
-    ModalFooter
-} from './styled';
+  ModalWrapper,
+  ModalOverlay,
+  ModalButton,
+  ModalHeader,
+  ModalTitle,
+  ModalPages,
+  ModalPageSection,
+  ModalFooter,
+} from "./styled";
 
-export default function Modal({setModalOpen}) {
+export default function Modal({ setModalOpen }) {
+  // 모달창 애니메이션
+  const [isVisible, setIsVisible] = useState(false);
+  // 모달 처음 마운트 됨을 설정
+  const [isMounted, setIsMounted] = useState(false);
 
-    // 모달창 애니메이션 
-    const [isVisible, setIsVisible] = useState(false);
-    // 모달 처음 마운트 됨을 설정
-    const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+    setIsVisible(true);
+  }, []);
 
+  // 모달창 닫기
+  const closeModal = () => {
+    setIsVisible(false);
+
+    setTimeout(() => {
+      setModalOpen(false);
+    }, 500);
+  };
+
+  // 모달창 밖에 눌렀을 때
+  const outSection = useRef();
+
+  const useOnClickOutside = (ref, handler) => {
     useEffect(() => {
-        setIsMounted(true);
-        setIsVisible(true);
-    }, []);
+      const listener = (e) => {
+        if (!ref.current || ref.current.contains(e.target)) {
+          return;
+        }
+        handler();
+      };
 
-    
-    // 모달창 닫기
-    const closeModal = () => {
-        
-        setIsVisible(false);
+      document.addEventListener("mousedown", listener);
+      document.addEventListener("touchstart", listener);
 
-        setTimeout(()=> {
-            setModalOpen(false);
-        }, 500)
-    };
+      return () => {
+        document.removeEventListener("mousedown", listener);
+        document.removeEventListener("touchstart", listener);
+      };
+    });
+  };
 
+  useOnClickOutside(outSection, closeModal);
 
-    // 모달창 밖에 눌렀을 때
-    const outSection = useRef();
+  // router
+  const router = useRouter();
 
-    const useOnClickOutside = (ref, handler) => {
-    useEffect(() => {
-        const listener = (e) => {
-            if (!ref.current || ref.current.contains(e.target)) {
-                return;
-            }
-            handler();
-        };
-    
-        document.addEventListener('mousedown', listener);
-        document.addEventListener('touchstart', listener);
+  // 모달이 완전히 열린 후 렌더링
+  if (!isMounted) {
+    return null;
+  }
 
-        return () => {
-          document.removeEventListener('mousedown', listener);
-          document.removeEventListener('touchstart', listener);
-        };
-      });
-};
-
-    useOnClickOutside(outSection, closeModal);
-
-    // router
-    const router = useRouter();
-
-    // 모달이 완전히 열린 후 렌더링
-    if (!isMounted) {
-        return null;
-    }
-
-    return(
-        <>
-            <ModalOverlay />
-            <ModalWrapper ref={outSection} isVisible={isVisible}>
-                <ModalButton icon={faArrowLeftLong} onClick={closeModal} />
-                <ModalHeader>
-                    <ModalTitle>동국대학교<br/>봄 백상대동제</ModalTitle>
-                    <ModalTitle><Image src={nav_logo} alt="nav_logo"/></ModalTitle>
-                </ModalHeader>
-                <ModalPages>
-                    <Link href="/" passHref>
-                        <ModalPageSection
-                            is_active = {router.pathname === '/'}
-                            onClick={closeModal}
-                            >main</ModalPageSection>
-                    </Link>
-                    <Link href="/notice" passHref>
-                        <ModalPageSection
-                            is_active = {router.pathname === '/notice'}
-                            onClick={closeModal}
-                            >notice</ModalPageSection>
-                    </Link>
-                    <Link href="/timetable" passHref>
-                        <ModalPageSection
-                            is_active = {router.pathname === '/timetable'}
-                            onClick={closeModal}
-                            >timetable</ModalPageSection>
-                    </Link>
-                    <Link href="/booth" passHref>
-                        <ModalPageSection
-                            is_active = {router.pathname === '/booth'}
-                            onClick={closeModal}
-                            >booth</ModalPageSection>
-                    </Link>
-                    <Link href="/about" passHref>
-                        <ModalPageSection
-                            is_active = {router.pathname === '/about'}
-                            onClick={closeModal}
-                            >about</ModalPageSection>
-                    </Link>
-                </ModalPages>
-                <ModalFooter>©DGU LIKELION. All rights reserved.</ModalFooter>
-            </ModalWrapper>
-        </>
-        
-    );
+  return (
+    <>
+      <ModalOverlay />
+      <ModalWrapper ref={outSection} isVisible={isVisible}>
+        <ModalButton icon={faArrowLeftLong} onClick={closeModal} />
+        <ModalHeader>
+          <ModalTitle>
+            동국대학교
+            <br />봄 백상대동제
+          </ModalTitle>
+          <ModalTitle>
+            <Image src={nav_logo} alt="nav_logo" />
+          </ModalTitle>
+        </ModalHeader>
+        <ModalPages>
+          <Link href="/" passHref>
+            <ModalPageSection is_active={router.pathname === "/"} onClick={closeModal}>
+              main
+            </ModalPageSection>
+          </Link>
+          <Link href="/notice" passHref>
+            <ModalPageSection is_active={router.pathname === "/notice"} onClick={closeModal}>
+              notice
+            </ModalPageSection>
+          </Link>
+          <Link href="/timetable" passHref>
+            <ModalPageSection is_active={router.pathname === "/timetable"} onClick={closeModal}>
+              timetable
+            </ModalPageSection>
+          </Link>
+          <Link href="/booth" passHref>
+            <ModalPageSection is_active={router.pathname === "/booth"} onClick={closeModal}>
+              booth
+            </ModalPageSection>
+          </Link>
+          <Link href="/about" passHref>
+            <ModalPageSection is_active={router.pathname === "/about"} onClick={closeModal}>
+              about
+            </ModalPageSection>
+          </Link>
+        </ModalPages>
+        <ModalFooter>©DGU LIKELION. All rights reserved.</ModalFooter>
+      </ModalWrapper>
+    </>
+  );
 }
--- components/nav/styled.jsx
+++ components/nav/styled.jsx
@@ -2,7 +2,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled, { keyframes, css } from "styled-components";
 import { motion } from "framer-motion";
 
-
 export const slideIn = keyframes`
     
     from {
@@ -27,82 +26,81 @@ export const slideOut = keyframes`
 `;
 
 export const ModalWrapper = styled.div`
+  background: #ffffff;
+  z-index: 150;
+  height: 100vh;
+  top: 0;
+  position: absolute;
+  border-radius: 20px 0px 0px 20px;
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 70%;
+  right: 0;
+  /* visibility: hidden; */
+  /* visibility: ${(props) => (!(props.isVisible || props.modalOpen) ? "hidden" : "visible")}; */
 
-    background: #FFFFFF;
-    z-index: 100;
-    height: 100vh;
-    top: 0;
-    position: absolute;
-    border-radius: 20px 0px 0px 20px;
-    padding: 30px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    width: 70%;
-    right: 0;
-    /* visibility: hidden; */
-    /* visibility: ${(props)=> !(props.isVisible || props.modalOpen)? 'hidden' : 'visible'}; */
-
-    /* display: ${(props) => props.modalOpen? "flex" : "none" }; */
-    /* opacity: ${(props) => ( props.isVisible ? 1 : 0)}; */
-    animation-name: ${(props) => (props.isVisible ? slideIn : slideOut) };
-    animation-duration: 0.5s;
-    /* animation-fill-mode: forwards; */
+  /* display: ${(props) => (props.modalOpen ? "flex" : "none")}; */
+  /* opacity: ${(props) => (props.isVisible ? 1 : 0)}; */
+  animation-name: ${(props) => (props.isVisible ? slideIn : slideOut)};
+  animation-duration: 0.5s;
+  /* animation-fill-mode: forwards; */
 `;
 
 export const ModalOverlay = styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: #101010CC;
-    z-index: 99; 
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #101010cc;
+  z-index: 149;
 `;
 
 export const ModalButton = styled(FontAwesomeIcon)`
-    width: 20px;
-    height: 5vh;
+  width: 20px;
+  height: 5vh;
 `;
 
 export const ModalHeader = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    border-bottom: 1px solid #D2D2D2;
-    padding: 3.5vh 0 4vh 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #d2d2d2;
+  padding: 3.5vh 0 4vh 0;
 `;
 
 export const ModalTitle = styled.ul`
-    line-height: 2.5vh;
-    color: #525252;
+  line-height: 2.5vh;
+  color: #525252;
 `;
 
 export const ModalPages = styled.div`
-    padding: 10vh 0;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    gap: 4vh;
-    font-weight: 700;
-    font-size: 32px;
+  padding: 10vh 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 4vh;
+  font-weight: 700;
+  font-size: 32px;
 `;
 
 export const ModalPageSection = styled.a`
-    width: 100%;
-    display: flex;
-    color: ${ ({is_active}) => ( !is_active ? '#525252' : '#FC8CAE') };
+  width: 100%;
+  display: flex;
+  color: ${({ is_active }) => (!is_active ? "#525252" : "#FC8CAE")};
 
-    &:hover {
-        color: #FC8CAE;
-    }
+  &:hover {
+    color: #fc8cae;
+  }
 `;
 
 export const ModalFooter = styled.footer`
-    padding: 7vh 0;
-    display: flex;
-    font-weight: 500;
-    font-size: 12px;
-    color: #BBBBBB;
+  padding: 7vh 0;
+  display: flex;
+  font-weight: 500;
+  font-size: 12px;
+  color: #bbbbbb;
 `;
